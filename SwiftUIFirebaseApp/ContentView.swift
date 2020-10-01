d306b42d29a5c69dbde18eef12511f41bc9b695c
Jack <JackAtGitHub@gmail.com>
2020-10-01 15:45:19 +0100

    Remove GeometryReader.

--- SwiftUIFirebaseApp/ContentView.swift
+++ SwiftUIFirebaseApp/ContentView.swift
@@ -12,9 +12,10 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             VStack {
-                Login()
+                Spacer()
+                Register()
+                Spacer()
             }
-            .padding(.top, 30)
         }
         .background(Color("DefaultGreen"))
         .edgesIgnoringSafeArea(.all)
@@ -38,11 +39,8 @@ struct Login: View {
     var body: some View {
         
         ZStack(alignment: .topTrailing) {
-            GeometryReader { geometry in
                 VStack {
-                    
-                    Spacer()
-                    
+                                        
                     Image("image_bird")
                         .resizable()
                         .scaledToFit()
@@ -107,10 +105,8 @@ struct Login: View {
                     .cornerRadius(10)
                     .padding(.top, 25)
                     
-                    Spacer()
                 }
                 .padding(.horizontal, 25)
-            }
             
             Button(action: {
                 
@@ -122,5 +118,105 @@ struct Login: View {
             .padding()
         }
     }
+}
+
+struct Register: View {
+    @State private var color = Color.white.opacity(0.7)
+    @State private var email = ""
+    @State private var password = ""
+    @State private var passwordConfirm = ""
+    @State private var visible = false
+    @State private var revisible = false
     
+    var body: some View {
+        
+        ZStack(alignment: .topLeading) {
+                VStack {
+                                        
+                    Image("image_bird")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200.0, height: 150.0)
+                    
+                    Text("Create a new account")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    TextField("Email", text: self.$email)
+                        .padding()
+                        .background(RoundedRectangle(cornerRadius: 4).stroke(self.email != "" ? Color("Colour") : self.color, lineWidth: 2))
+                        .padding(.top, 25)
+                        .foregroundColor(.white)
+                    
+                    HStack(spacing: 15) {
+                        VStack {
+                            if self.visible {
+                                TextField("Password", text: self.$password)
+                            } else {
+                                SecureField("Password", text: self.$password)
+                            }
+                        }
+                        
+                        Button(action: {
+                            
+                            self.visible.toggle()
+                            
+                        }) {
+                            Image(systemName: self.visible ? "eye.slash.fill" : "eye.fill")
+                                .foregroundColor(self.color)
+                        }
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.passwordConfirm != "" ? Color("Colour") : self.color, lineWidth: 2))
+                    .padding(.top, 25)
+                    .foregroundColor(.white)
+                    
+                    HStack(spacing: 15) {
+                        VStack {
+                            if self.revisible {
+                                TextField("Re-enter Password", text: self.$passwordConfirm)
+                            } else {
+                                SecureField("Re-enter Password", text: self.$passwordConfirm)
+                            }
+                        }
+                        
+                        Button(action: {
+                            
+                            self.revisible.toggle()
+                            
+                        }) {
+                            Image(systemName: self.revisible ? "eye.slash.fill" : "eye.fill")
+                                .foregroundColor(self.color)
+                        }
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 4).stroke(self.password != "" ? Color("Colour") : self.color, lineWidth: 2))
+                    .padding(.top, 25)
+                    .foregroundColor(.white)
+                    
+                    Button(action: {
+                        
+                    }) {
+                        Text("Register")
+                            .foregroundColor(Color("DefaultGreen"))
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width - 50)
+                    }
+                    .background(Color.white)
+                    .cornerRadius(10)
+                    .padding(.top, 25)
+                }
+                .padding(.horizontal, 25)
+            
+            Button(action: {
+                
+            }) {
+                Image(systemName: "chevron.left")
+                    .font(.title)
+                    .foregroundColor(.white)
+            }
+            .padding()
+        }
+    }
 }
